b8faf936ec248de036150305b287a1d5a3eb3da0
NuN <144636106+NunezOFC@users.noreply.github.com>
2024-10-22 12:06:56 -0300

    Add Sum Calculator
    Print CLI sum of two numbers

---
+++ Calculator.py
@@ -0,0 +1,9 @@
+#pegar os números
+x = input("digite o primeiro número: ")
+y = input("digite o segundo número: ")
+
+#calcula os números escolhidos
+z = int(x)+int(y)
+
+#resultado
+print(f"a soma de {x} + {y} = {z}")
