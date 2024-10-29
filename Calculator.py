2cd3a8f4e9d3b18569ca9964a0945850b1e78917
NuN <144636106+NunezOFC@users.noreply.github.com>
2024-10-29 12:28:16 -0300

    Update Calculator.py
    Print 5 Operation Terminal Calculator

--- Calculator.py
+++ Calculator.py
@@ -1,37 +1,49 @@
-#Get the Numbers
-x = input("Write the First Number:")
-y = input("Write the Second Number:")
+#Calculator
+def calculator():
+   #Get the numbers
+    x = float(input("Write the First Number:"))
+    y = float(input("Write the Second Number:"))
 
-#Choice of Operation
-print("Choose your Operation")
-print("1 for Division")
-print("2 for Abstraction")
-print("3 for Multiplication")
-print("4 for Sum")
-print("5 for Exponenciation")
-operation = int(input("Write your Operation:"))
+    #Choice of Operation
+    print("Choose your Operation")
+    print("1 for Division")
+    print("2 for Abstraction")
+    print("3 for Multiplication")
+    print("4 for Sum")
+    print("5 for Exponenciation")
+    operation = int(input("Write your Operation:"))
 
-#Calculates the chosen numbers
-if operation == 1:
-     z = int(x)/int(y)
-elif operation == 2:
-    z = int(x)-int(y)
-elif operation == 3:
-    z = int(x)*int(y)
-elif operation == 4:
-    z = int(x)+int(y)
-elif operation == 5:
-    z = int(x)**int(y)
+    #Calculates the chosen numbers
+    if operation == 1:
+        z = (x)/(y)
+        z = f"{z:_.2f}"
+        z = z.replace(".", ",").replace("_",".")
+    elif operation == 2:
+        z = (x)-(y)
+        z = f"{z:_.2f}"
+        z = z.replace(".", ",").replace("_",".")
+    elif operation == 3:
+        z = (x)*(y)
+        z = f"{z:_.2f}"
+    elif operation == 4:
+        z = (x)+(y)
+        z = f"{z:_.2f}"
+        z = z.replace(".", ",").replace("_",".")
+    elif operation == 5:
+        z = (x)**(y)
+        z = f"{z:_.2f}"
+        z = z.replace(".", ",").replace("_",".")
 
-#Result
-if operation == 1:
-    print(f"The Division between {x} and {y} is {z}")
-elif operation == 2:
-    print(f"The Abstraction between {x} and {y} is {z}")
-elif operation == 3:
-    print(f"The Multiplication between {x} and {y} is {z}")
-elif operation == 4:
-    print(f"The Sum between {x} and {y} is {z}")
-elif operation == 5:
-     print(f"The Exponenciation between {x} and {y} is {z}")
+    #Result
+    if operation == 1:
+        print(f"The Division between {x} and {y} is {z}")
+    elif operation == 2:
+        print(f"The Abstraction between {x} and {y} is {z}")
+    elif operation == 3:
+        print(f"The Multiplication between {x} and {y} is {z}")
+    elif operation == 4:
+        print(f"The Sum between {x} and {y} is {z}")
+    elif operation == 5:
+        print(f"The Exponenciation between {x} and {y} is {z}")
 
+calculator()
