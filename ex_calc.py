8fccd6850660e778fa850868a679021c04a330d1
NuN <144636106+NunezOFC@users.noreply.github.com>
2024-11-26 12:24:33 -0300

    Add Expression Calculator
    Add CLI almost complete expression Calculator

---
+++ ex_calc.py
@@ -0,0 +1,43 @@
+result= None
+result2 = None 
+#Escolha da Expressão
+def ex():
+    print("Escreva a sua operação/Exemplo (2+1) ")
+    ex = input("")
+    sep(ex)
+
+#Separação da expressão
+def sep(ex):
+    ex = ex.replace("+", " + ")
+    ex = ex.replace("-", " - ")
+    ex = ex.replace("/", " / ")
+    ex = ex.replace("**", " ** ")
+    ex = ex.replace("*", " * ")
+    ex = ex.split()
+    size = len(ex)
+    if size == 3:
+        cal3(ex, size)
+    elif size == 5:
+        cal5(ex, size)
+
+#Calculus
+def cal3(ex):
+    if ex[1] == "+":
+        result = int(ex[0]) + int(ex[2])
+        print(f"The result of {ex[0]} + {ex[2]} = {result}")
+    elif ex[1] == "-":
+        result = int(ex[0]) - int(ex[2])
+        print(f"The result of {ex[0]} - {ex[2]} = {result}")
+    elif ex[1] == "*":
+        result = int(ex[0]) * int(ex[2])
+        print(f"The result of {ex[0]} x {ex[2]} = {result}")
+    elif ex[1] == "/":
+        result = int(ex[0]) / int(ex[2])
+        print(f"The result of {ex[0]} / {ex[2]} = {result}")
+
+#Calculus
+def cal5(ex):
+    if ex[1] == "*":
+        result 
+
+ex()
