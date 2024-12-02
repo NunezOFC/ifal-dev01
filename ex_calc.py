0b0f0b2469eb4c250d2b22798eb6e0ef04b58d6a
NuN <144636106+NunezOFC@users.noreply.github.com>
2024-12-01 21:01:41 -0300

    Update ex_calc.py
    Added Not Fully Five 2 Operation Expression calculator

--- ex_calc.py
+++ ex_calc.py
@@ -2,6 +2,8 @@ result= None
 result2 = None 
 #Escolha da Expressão
 def ex():
+    print("Use * para multiplicaçao e ** para exponenciaçao")
+    print("")
     print("Escreva a sua operação/Exemplo (2+1) ")
     ex = input("")
     sep(ex)
@@ -11,14 +13,14 @@ def sep(ex):
     ex = ex.replace("+", " + ")
     ex = ex.replace("-", " - ")
     ex = ex.replace("/", " / ")
-    ex = ex.replace("**", " ** ")
+    ex = ex.replace("**", " ^ ")
     ex = ex.replace("*", " * ")
     ex = ex.split()
     size = len(ex)
     if size == 3:
         cal3(ex)
     elif size == 5:
-        cal5(ex, size)
+        cal5(ex)
 
 #Calculus
 def cal3(ex):
@@ -34,10 +36,70 @@ def cal3(ex):
     elif ex[1] == "/":
         result = float(ex[0]) / float(ex[2])
         print(f"The result of {ex[0]} / {ex[2]} = {result}")
+    elif ex[1] == "^":
+        result = float(ex[0]) ** float(ex[2])
+        print(f"The result of {ex[0]} ^ {ex[2]} = {result}")
 
 #Calculus
 def cal5(ex):
-    if ex[1] == "*":
-        result 
+    if ex[3] == "^":
+        result = float(ex[2]) ** float(ex[4])
+        if ex[1] == "+":
+            result2 = float(result) + float(ex[0])
+            print(f"The result of {ex[0]} + {ex[2]} ^ {ex[4]} = {result2}")
+        elif ex[1] == "-":
+            result2 = float(result) - float(ex[0])
+            print(f"The result of {ex[0]} - {ex[2]} ^ {ex[4]} = {result2}")
+        elif ex[1] == "*":
+            result2 = float(result) * float(ex[0])
+            print(f"The result of {ex[0]} x {ex[2]} ^ {ex[4]} = {result2}")
+        elif ex[1] == "/":
+            result2 = float(result) / float(ex[0])
+            print(f"The result of {ex[0]} / {ex[2]} ^ {ex[4]} = {result2}")
+        elif ex[1] == "^":
+            result2 = float(result) ** float(ex[0])
+            print(f"The result of {ex[0]} ^ {ex[2]} ^ {ex[4]} = {result2}")
+    elif ex[1] == "^":
+        result = float(ex[0]) ** float(ex[2])
+        if ex[3] == "+":
+            result2 = float(result) + float(ex[4])
+            print(f"The result of {ex[0]} ^ {ex[2]} + {ex[4]} = {result2}")
+        elif ex[3] == "-":
+            result2 = float(result) - float(ex[4])
+            print(f"The result of {ex[0]} ^ {ex[2]} - {ex[4]} = {result2}")
+        elif ex[3] == "*":
+            result2 = float(result) * float(ex[4])
+            print(f"The result of {ex[0]} ^ {ex[2]} * {ex[4]} = {result2}")
+        elif ex[3] == "/":
+            result2 = float(result) / float(ex[4])
+            print(f"The result of {ex[0]} ^ {ex[2]} / {ex[4]} = {result2}")
+    elif ex[1] == "*":
+        result = float(ex[0]) * float(ex[2])
+        if ex[3] == "+":
+            result2 = float(result) + float(ex[4])
+            print(f"The result of {ex[0]} x {ex[2]} + {ex[4]} = {result2}")
+        elif ex[3] == "-":
+            result2 = float(result) - float(ex[4])
+            print(f"The result of {ex[0]} x {ex[2]} - {ex[4]} = {result2}")
+        elif ex[3] == "*":
+            result2 = float(result) * float(ex[4])
+            print(f"The result of {ex[0]} x {ex[2]} * {ex[4]} = {result2}")
+        elif ex[3] == "/":
+            result2 = float(result) / float(ex[4])
+            print(f"The result of {ex[0]} x {ex[2]} / {ex[4]} = {result2}")
+    elif ex[1] == "/":
+        result = float(ex[0]) / float(ex[2])
+        if ex[3] == "+":
+            result2 = float(result) + float(ex[4])
+            print(f"The result of {ex[0]} / {ex[2]} + {ex[4]} = {result2}")
+        elif ex[3] == "-":
+            result2 = float(result) - float(ex[4])
+            print(f"The result of {ex[0]} / {ex[2]} - {ex[4]} = {result2}")
+        elif ex[3] == "*":
+            result2 = float(result) * float(ex[4])
+            print(f"The result of {ex[0]} / {ex[2]} * {ex[4]} = {result2}")
+        elif ex[3] == "/":
+            result2 = float(result) / float(ex[4])
+            print(f"The result of {ex[0]}  {ex[2]} / {ex[4]} = {result2}")
 
 ex()
